6ef6a37e54758b3e47d1065799e45d8461aae459
Olle Månsson <31876997+ollema@users.noreply.github.com>
2026-07-06 11:05:17 +0200

    falling squares

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,79 @@
 use macroquad::prelude::*;
 
-const MOVEMENT_SPEED: f32 = 200.0;
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
 
 #[macroquad::main("kurvfeber")]
 async fn main() {
-    let mut x = screen_width() / 2.0;
-    let mut y = screen_height() / 2.0;
-        
+    const MOVEMENT_SPEED: f32 = 200.0;
+
+    rand::srand(miniquad::date::now() as u64);
+
+    let mut squares = vec![];
+    let mut circle = Shape {
+        size: 32.0,
+        speed: MOVEMENT_SPEED,
+        x: screen_width() / 2.0,
+        y: screen_height() / 2.0,
+    };
+
     loop {
         clear_background(BLACK);
 
         let delta_time = get_frame_time();
-        
+
+        // player movement
         if is_key_down(KeyCode::Right) {
-            x += MOVEMENT_SPEED * delta_time;
+            circle.x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= MOVEMENT_SPEED * delta_time;
+            circle.x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += MOVEMENT_SPEED * delta_time;
+            circle.y += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= MOVEMENT_SPEED * delta_time;
+            circle.y -= MOVEMENT_SPEED * delta_time;
         }
 
-        x = clamp(x, 0.0, screen_width());
-        y = clamp(y, 0.0, screen_height());
+        // clamp player movement
+        circle.x = clamp(circle.x, 0.0, screen_width());
+        circle.y = clamp(circle.y, 0.0, screen_height());
 
-        draw_circle(x, y, 16.0, YELLOW);
+        // generate a new square
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                y: -size,
+            })
+        }
+
+        // move squares
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+
+        // remove squares
+        squares.retain(|square| square.y < screen_height() + square.size);
+
+        // draw everything
+        draw_circle(circle.x, circle.y, 16.0, YELLOW);
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size,
+                GREEN,
+            );
+        }
 
         next_frame().await
     }
