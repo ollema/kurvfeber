b591c33b6ce70ea54e4d26d5a001f3a9b5d628ea
Olle Månsson <31876997+ollema@users.noreply.github.com>
2026-07-06 13:22:58 +0200

    points

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::*;
+use std::fs;
 
 struct Shape {
     size: f32,
@@ -39,6 +40,11 @@ async fn main() {
     };
     let mut bullets: Vec<Shape> = vec![];
     let mut gameover = false;
+
+    let mut score: u32 = 0;
+    let mut high_score: u32 = fs::read_to_string("highscore.dat")
+        .map_or(Ok(0), |i| i.parse::<u32>())
+        .unwrap_or(0);
 
     loop {
         clear_background(BLACK);
@@ -109,6 +115,9 @@ async fn main() {
 
         // check for collisions
         if squares.iter().any(|square| circle.collides_with(square)) {
+            if score == high_score {
+                fs::write("highscore.dat", high_score.to_string()).ok();
+            }
             gameover = true;
         }
         for square in squares.iter_mut() {
@@ -116,6 +125,8 @@ async fn main() {
                 if bullet.collides_with(square) {
                     bullet.collided = true;
                     square.collided = true;
+                    score += square.size.round() as u32;
+                    high_score = high_score.max(score);
                 }
             }
         }
@@ -126,6 +137,7 @@ async fn main() {
             bullets.clear();
             circle.x = screen_width() / 2.0;
             circle.y = screen_height() / 2.0;
+            score = 0;
             gameover = false;
         }
 
@@ -143,6 +155,22 @@ async fn main() {
                 GREEN,
             );
         }
+        draw_text(
+            format!("Score: {}", score).as_str(),
+            10.0,
+            35.0,
+            25.0,
+            WHITE,
+        );
+        let highscore_text = format!("High score: {}", high_score);
+        let text_dimensions = measure_text(highscore_text.as_str(), None, 25, 1.0);
+        draw_text(
+            highscore_text.as_str(),
+            screen_width() - text_dimensions.width - 10.0,
+            35.0,
+            25.0,
+            WHITE,
+        );
         if gameover {
             let text = "GAME OVER!";
             let text_dimensions = measure_text(text, None, 50, 1.0);
